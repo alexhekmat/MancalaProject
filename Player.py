5306379cf459d96882e972a855dd05f5fea2f943
Ymaelaf <ymaelaf@potomacschool.org>
2022-10-12 08:53:56 -0400

    Player update

--- Player.py
+++ Player.py
@@ -1,1 +1,16 @@
 class Player:
+
+
+def __init__(self, hr, hl):
+    self.gameboard = [2,2]
+    self.hole_right = 2
+    self.hole_left = 2
+
+##This constructor will construct the amount of seeds in each hole for the player
+def get_right(self)
+##This method will access the amount of seeds in the left hole
+def change_right_hole(self, hr)
+##This method will mutate the amount of seeds in the right hole
+def change_left_hole(self, hl)
+##This method will mutate the amount of seeds in the left hole
+
