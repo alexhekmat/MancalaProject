51f0e814feb3ed3f817a84a66dd066ada1a909be
Alex Hek <alexanderhekmat@gmail.com>
2022-10-12 09:04:54 -0400

    player constructor  update

--- Player.py
+++ Player.py
@@ -1,17 +1,17 @@
 class Player:
 
 
-def __init__(self):
-    self.hole_right = 2
-    self.hole_left = 2
-    self.gameboard = [self.hole_right, self.hole_left]
+    def __init__(self):
+        self.hole_right = 2
+        self.hole_left = 2
+        self.gameboard = [self.hole_right, self.hole_left]
 
 
-##This constructor will construct the amount of seeds in each hole for the player
-def get_right(self):
-##This method will access the amount of seeds in the left hole
-def change_right_hole(self, hr):
-##This method will mutate the amount of seeds in the right hole
-def change_left_hole(self, hl):
-##This method will mutate the amount of seeds in the left hole
+    ##This constructor will construct the amount of seeds in each hole for the player
+    def get_right(self):
+    ##This method will access the amount of seeds in the left hole
+    def change_right_hole(self, hr):
+    ## This method will mutate the amount of seeds in the right hole
+    def change_left_hole(self, hl):
+    ## This method will mutate the amount of seeds in the left hole
 
