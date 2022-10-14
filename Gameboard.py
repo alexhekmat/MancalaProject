fa4bf8da69589f6ef897af3a4a5b3984189ac2a6
Ymaelaf <ymaelaf@potomacschool.org>
2022-10-14 09:10:48 -0400

    Gameboard update

--- Gameboard.py
+++ Gameboard.py
@@ -1,11 +1,13 @@
 class Gameboard:
 
     def __init__(self,p1,p2):
-        self.player1
+        self.player1 = p1
+        self.player2 = p2
 
     def sow_seed(self, player):
-    def checks_winner(self, player1, player2):
-
-    def print_board(self):
+    def checks_winner(self, player):
+        if ( player.get_Right() == 0) and (player.get_left() == 0):
+            return "no move"
+    def print_board(self, player1, player2):
         print([player1.get_right(), player1.get_left()])
         print([player2.get_left(), player2.get_left()])
