314106de7a372835afe5fe7bd7df0d82f2ff521a
Alex Hek <alexanderhekmat@gmail.com>
2022-10-14 09:05:49 -0400

    day 2 update

--- Gameboard.py
+++ Gameboard.py
@@ -1,9 +1,11 @@
 class Gameboard:
 
     def __init__(self,p1,p2):
-        self.
+        self.player1
 
     def sow_seed(self, player):
     def checks_winner(self, player1, player2):
 
     def print_board(self):
+        print([player1.get_right(), player1.get_left()])
+        print([player2.get_left(), player2.get_left()])
--- Human.py
+++ Human.py
@@ -1,7 +1,4 @@
 from Player import Player
 
-
 class Human(Player):
     pass
-#changes
-#changes2
