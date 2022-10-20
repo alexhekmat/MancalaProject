ac0108c2dc31efbbb7882c8c3f2412f39a8514e7
Ymaelaf <ymaelaf@potomacschool.org>
2022-10-20 08:48:27 -0400

    client update

--- MancalaClient.py
+++ MancalaClient.py
@@ -5,5 +5,9 @@ player1 = Human()
 player2 = Human()
 gameboard = Gameboard(player1, player2)
 
+gameboard.print_board(player1, player2)
+
 choice = player1.choose_hole()
 gameboard.sow_seed(player1, player2, choice)
+choice = player2.choose_hole()
+gameboard.sow_seed(player2, player1, choice)
