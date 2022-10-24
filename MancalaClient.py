db649fc246289f5a002ff48b1521ec3e47c25073
Ymaelaf <ymaelaf@potomacschool.org>
2022-10-24 09:36:47 -0400

    client update

--- MancalaClient.py
+++ MancalaClient.py
@@ -9,13 +9,16 @@ gameboard.print_board(player1, player2)
 
 
 while (gameboard.checks_winner(player1)) and (gameboard.checks_winner(player2)):
+    while seeds == 0:
+         print("Invalid choice, pick hole again")
     if gameboard.checks_winner(player1):
         choice = player1.choose_hole()
         gameboard.sow_seed(player1, player2, choice)
         gameboard.print_board(player1, player2)
     else:
         print("Player 2 Wins")
-
+    while seeds == 0:
+        ("Invalid choice, pick hole again")
     if gameboard.checks_winner(player2):
         choice = player2.choose_hole()
         gameboard.sow_seed(player2, player1, choice)
