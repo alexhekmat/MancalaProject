a10c6dceb57a6093d822da1e76576802d485a001
Ymaelaf <ymaelaf@potomacschool.org>
2022-10-26 08:32:43 -0400

    While loop Client Update

--- MancalaClient.py
+++ MancalaClient.py
@@ -9,18 +9,27 @@ gameboard.print_board(player1, player2)
 
 
 while (gameboard.checks_winner(player1)) and (gameboard.checks_winner(player2)):
-    while seeds == 0:
-         print("Invalid choice, pick hole again")
+
     if gameboard.checks_winner(player1):
         choice = player1.choose_hole()
+        while choice == 0 and player1.get_right() == 0:
+            print("Invalid choice, pick hole again")
+            choice = player1.choose_hole()
+        while choice == 1 and player1.get_left() == 0:
+            print("Invalid choice, pick hole again")
+            choice = player1.choose_hole()
         gameboard.sow_seed(player1, player2, choice)
         gameboard.print_board(player1, player2)
     else:
         print("Player 2 Wins")
-    while seeds == 0:
-        ("Invalid choice, pick hole again")
     if gameboard.checks_winner(player2):
         choice = player2.choose_hole()
+        while choice == 0 and player2.get_right() == 0:
+            print("Invalid choice, pick hole again")
+            choice = player2.choose_hole()
+        while choice == 1 and player2.get_left() == 0:
+            print("Invalid choice, pick hole again")
+            choice = player1.choose_hole()
         gameboard.sow_seed(player2, player1, choice)
         gameboard.print_board(player1, player2)
     else:
