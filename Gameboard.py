8f390a066cdc24f496adf13a7cdc2bdb18ae16d5
Alex Hek <alexanderhekmat@gmail.com>
2022-10-30 18:10:08 -0400

    day 7 comments

--- Gameboard.py
+++ Gameboard.py
@@ -1,48 +1,71 @@
 class Gameboard:
 
+    ## This constructor will construct the player objects and set them to whichever players are playing
+    ## Param: p1 is a player object
+    ## Param: p2 is a player object
     def __init__(self, p1, p2):
         self.player1 = p1
         self.player2 = p2
 
+    ##This method will sow the seed from whichever hole the player chooses
+    ## Param: p1 is a player object
+    ## Param: p2 is a player object
+    ## Param: choice is an int indicating which hole
     def sow_seed(self, p1, p2, choice):
+        ## Checks if they chose the right hole or the left hole using the int 0 or 1
         if choice == 0:
             seeds = p1.get_right()
             p1.empty_hole(choice)
+            ## Runs the while until seeds = 0
             while seeds > 0:
                 p2.change_left_hole()
                 seeds = seeds - 1
+                ## Runs in a counter clockwise order for each hole using an if to determine if it should be placed in the hole
                 if seeds > 0:
                     p2.change_right_hole()
                     seeds = seeds - 1
+                ## Runs in a counter clockwise order for each hole using an if to determine if it should be placed in the hole
                 if seeds > 0:
                     p1.change_left_hole()
                     seeds = seeds - 1
+                ## Runs in a counter clockwise order for each hole using an if to determine if it should be placed in the hole
                 if seeds > 0:
                     p1.change_right_hole()
                     seeds = seeds - 1
         else:
             seeds = p1.get_left()
             p1.empty_hole(choice)
+            ## Runs the while until seeds = 0
             while seeds > 0:
                 p1.change_right_hole()
                 seeds = seeds - 1
+                ## Runs in a counter clockwise order for each hole using an if to determine if it should be placed in the hole
                 if seeds > 0:
                     p2.change_left_hole()
                     seeds = seeds - 1
+                ## Runs in a counter clockwise order for each hole using an if to determine if it should be placed in the hole
                 if seeds > 0:
                     p2.change_right_hole()
                     seeds = seeds - 1
+                ## Runs in a counter clockwise order for each hole using an if to determine if it should be placed in the hole
                 if seeds > 0:
                     p1.change_left_hole()
                     seeds = seeds - 1
 
-    ## if False is returned than the player has lost, if True is returned then continue play
+
+    ## This method checks if the player parameters holes both have zero seeds in them, if it returns false they do, if not true
+    ## Param: player is a player object
+    ## Returns: a boolean either True or False
     def checks_winner(self, player):
+        ## Checks if the players right and left holes are both zero, returning false if they are, and true if they are not
         if (player.get_right() == 0) and (player.get_left() == 0):
             return False
         else:
             return True
 
+    ## This method prints the 2 arrays to print the board showcasing it from a top down view
+    ## Param: player1 is a player object
+    ## Param: player2 is a player object
     def print_board(self, player1, player2):
         print([player1.get_right(), player1.get_left()])
         print([player2.get_left(), player2.get_right()])
--- MancalaClient.py
+++ MancalaClient.py
@@ -8,13 +8,17 @@ print("The Mini Mancala board is viewed from a top down angle, therefore the fir
       "views the top half of the board reversed")
 gameboard.print_board(player1, player2)
 
+## While loop runs with an or because there is a break within the winner, instead of an and, so it runs until reaching the break
 while (gameboard.checks_winner(player1)) or (gameboard.checks_winner(player2)):
+    ## Checks if the player1 holes still have seeds by calling checking winner, else it prints that the second player won and breaks
     if gameboard.checks_winner(player1):
         print("First Player Choose a Hole!")
         choice = player1.choose_hole()
+        ## While loop in order to check if there are seeds in the hole
         while choice == 0 and player1.get_right() == 0:
             print("Invalid choice, pick hole again")
             choice = player1.choose_hole()
+        ## While loop in order to check if there are seeds in the other hole
         while choice == 1 and player1.get_left() == 0:
             print("Invalid choice, pick hole again")
             choice = player1.choose_hole()
@@ -23,12 +27,15 @@ while (gameboard.checks_winner(player1)) or (gameboard.checks_winner(player2)):
     else:
         print("Second Player Wins")
         break
+    ## Checks if the player2 holes still have seeds by calling checking winner, else it prints that the first player won and breaks
     if gameboard.checks_winner(player2):
         print("Second Player Choose a Hole!")
         choice = player2.choose_hole()
+        ## While loop in order to check if there are seeds in the hole
         while choice == 0 and player2.get_right() == 0:
             print("Invalid choice, pick hole again")
             choice = player2.choose_hole()
+        ## While loop in order to check if there are seeds in the other hole
         while choice == 1 and player2.get_left() == 0:
             print("Invalid choice, pick hole again")
             choice = player1.choose_hole()
